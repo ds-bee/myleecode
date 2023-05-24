865169c7c4c43f60bfa19a2efc3665fd025b211d
hyq19 <10775197514@qq.com>
2023-05-24 20:31:28 +0800

    贪心算法,跳跃2，两个限制来解决这个问题，第一个限制记录当前步数最远能到达的位置，第二个限制记录这个步数中最远索引的下标，到了最远的地方就记录对应位置的         for (int i = 0; i < length - 1; i++) {             maxPosition = Math.max(maxPosition, i + nums[i]);             if (i == end) {                 end = maxPosition;                 steps++;             }         } 还有一个原因，我做的时候考虑了不能到达的情况，这道题是必能到达的。

--- src/com/huang/medium/jump/Solution.java
+++ src/com/huang/medium/jump/Solution.java
@@ -1,4 +1,43 @@
 package com.huang.medium.jump;
 
 public class Solution {
+    public static void main(String[] args) {
+        int[] arr ={3,2,1,0,4};
+        System.out.println(jump(arr));
+
+    }
+
+
+    public static int jump(int[] nums) {
+        int length = nums.length;
+        int end = 0;
+        int maxPosition = 0;
+        int steps = 0;
+        for (int i = 0; i < length - 1; i++) {
+            maxPosition = Math.max(maxPosition, i + nums[i]);
+            if (i == end) {
+                end = maxPosition;
+                steps++;
+            }
+        }
+        return steps;
+    }
+//    public static void find(int[] nums, int sum, int start, int record){
+//        if((nums[start] + start) >= nums.length - 1 ){
+//            sum++;
+//            if(sum < record){
+//                record = sum;
+//            }
+//            return;
+////            return record;
+//        }
+//        for (int i = start; i < nums.length - 1; i++) {
+//
+//            sum++;
+//            find(nums,sum,start+1,record);
+//            sum--;
+//        }
+////        return record;
+//    }
+
 }
