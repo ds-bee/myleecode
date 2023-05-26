e240a6e65ab05a240c2358c5352947f08256e8c1
hyq19 <10775197514@qq.com>
2023-05-26 11:09:11 +0800

    还是没有贪心成功

--- src/com/huang/medium/canCompleteCircuit/Solution.java
+++ src/com/huang/medium/canCompleteCircuit/Solution.java
@@ -9,8 +9,10 @@ public class Solution {
 //        int[] arr2 = {3,4,3};
 //        int[] arr1 = {1,2,3,4,5};
 //        int[] arr2 = {3,4,5,1,2};
-        int[] arr1 = {5,1,2,3,4};
-        int[] arr2 = {4,4,1,5,1};
+        int[] arr1 = {5,8,2,8};
+        int[] arr2 = {6,5,6,6};
+//        int[] arr1 = {5,1,2,3,4};
+//        int[] arr2 = {4,4,1,5,1};
         System.out.println(canCompleteCircuit(arr1, arr2));
     }
     public static int canCompleteCircuit(int[] gas, int[] cost) {
@@ -19,32 +21,39 @@ public class Solution {
         boolean flag = false;
         int record = 0;
         int sum = 0;
+        int remain = 0;
         ArrayList<Integer> listpre = new ArrayList<>();
+        ArrayList<Integer> listcur = new ArrayList<>();
         for (int i = 0; i < gas.length; i++) {
-            int remain = gas[i] - cost[i];
-            if(flag){
-                sum = sum + remain;
-            }
-            if(remain > 0 && !flag){
-                sum = sum + remain;
+//            listpre.add(gas[i] - cost[i]);
+            if(remain < gas[i] - cost[i]){
+                remain = gas[i] - cost[i];
                 record = i;
-                flag = true;
-            }
-            if(remain < 0 && !flag){
+            };
+
+        }
+        for (int i = 0; i < gas.length; i++) {
+            remain = gas[i] - cost[i];
+            if(record > i){
                 listpre.add(remain);
+            }else {
+                sum = sum + remain;
             }
-            if(sum <= 0 && flag){
-                return -1;
-            }
+//            if(sum <= 0){
+//                return -1;
+//            }
         }
         for (int i = 0; i < listpre.size(); i++) {
             sum = sum + listpre.get(i);
             if(i == listpre.size()-1 && sum == 0 ){
                 return record;
             }
-            if(sum <= 0){
+            if(sum <= 0 ){
                 return -1;
             }
+        }
+        if(sum <= 0 ){
+            return -1;
         }
         return record;
     }
