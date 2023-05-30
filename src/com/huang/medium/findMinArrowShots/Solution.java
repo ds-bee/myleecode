2e706a6311a830a37a4eafa37d00c869b126959b
hyq19 <10775197514@qq.com>
2023-05-30 20:42:49 +0800

    贪心成功，二维数组使用贪心一般是要进行排序，下来最好找些例子对数组和list进行排序的，然后就是需要去复习一下排序算法

--- src/com/huang/medium/findMinArrowShots/Solution.java
+++ src/com/huang/medium/findMinArrowShots/Solution.java
@@ -1,13 +1,17 @@
 package com.huang.medium.findMinArrowShots;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 
 public class Solution {
     public static void main(String[] args) {
-        int[][] arr = {{10, 16}, {2, 8}, {1, 6}, {7, 12}};
-//        int[][] arr = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+//        int[][] arr = {{10, 16}, {2, 8}, {1, 6}, {7, 12}};
+        int[][] arr = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+//        int[][] arr = {{-2147483646,-2147483645}, {2147483646,2147483647}};
+//        int[][] arr = {{-2,-1}, {1,2}};
+//        int[][] arr = {{1,2},{-2,-1}};
         int ints = findMinArrowShots(arr);
         System.out.println(ints);
     }
@@ -16,24 +20,15 @@ public class Solution {
         if (points.length == 0) {
             return 0;
         }
-        ArrayList<Integer> list = new ArrayList<>();
+//        long[][] pointslong =  points; 记住对二维数组排序使用这个方法就行了
+        Arrays.sort(points, Comparator.comparingInt(a -> a[1]));
+//        ArrayList<Integer> list = new ArrayList<>();
         int sum = 1;
-        list.add(points[0][0]);
-        list.add(points[0][1]);
+        long record = points[0][1];
         for (int i = 1; i < points.length; i++) {
-            Collections.sort(list);
-            int[] point = points[i];
-            if(point[1] < list.get(0)){
-                list.add(point[1] );
-                list.add(point[1] );
+            if(points[i][0] > record ){
+                record = points[i][1];
                 sum++;
-            }else if(point[0] > list.get(list.size() - 1)){
-                list.add(point[0]);
-                sum++;
-            }else{
-//                sum++;
-                list.add(point[0]);
-                list.add(point[1]);
             }
 
         }
