1005954bec5ccfe99003bb615391750d087d277b
hyq19 <10775197514@qq.com>
2023-06-01 10:28:18 +0800

    //分割数字         String[] strings = (N + "").split(""); 但是实际上还是贪心策略的问题，贪心没贪心对头 Integer.parseInt(String.join("",strings));

--- src/com/huang/medium/monotoneIncreasingDigits/Solution.java
+++ src/com/huang/medium/monotoneIncreasingDigits/Solution.java
@@ -6,44 +6,28 @@ import java.util.List;
 public class Solution {
     public static void main(String[] args) {
 //        int n = 101;
-        int n = 101;
-//        int n = 100;
+//        int n = 101;
+        int n = 100;
 //        int n = 332;
-//        int n = 1234;
+//        int n = 1231;
         System.out.println(monotoneIncreasingDigits(n));
     }
-    public static int monotoneIncreasingDigits(int n) {
-        int record = n;
-        ArrayList<Integer> list = new ArrayList<>();
-        while (record > 0){
-            list.add(record % 10);
-            record = record / 10;
-        }
-        boolean flag = false;
-//        while (!flag) {
-            for (int i = 0; i < list.size() - 1; i++) {
-                if (list.get(i) < list.get(i + 1) && list.get(i+1)!=0) {
-                    list.set(i, 9);
-                    list.set(i + 1, list.get(i + 1) - 1);
-                    continue;
-                }
-                if(list.get(i) < list.get(i + 1) && list.get(i+1)==0){
-                    list.set(i, 9);
-//                    list.set(i + 1, 9);
-                }
-                if (list.get(i) == list.get(i + 1) && list.get(i) == 0) {
-                    list.set(i, 9);
-                }
+    public static int monotoneIncreasingDigits(int N) {
+        //分割数字
+        String[] strings = (N + "").split("");
+        int start = strings.length;
+        for (int i = strings.length - 1; i > 0; i--) {
+            if (Integer.parseInt(strings[i]) < Integer.parseInt(strings[i - 1])) {
+                strings[i - 1] = (Integer.parseInt(strings[i - 1]) - 1) + "";
+                start = i;
             }
-//            flag = find(list);
-//        }
-        int a = 1;
-        for (int i = 0; i < list.size(); i++) {
-            record = list.get(i) * a + record;
-            a = a*10;
         }
-        return record;
+        for (int i = start; i < strings.length; i++) {
+            strings[i] = "9";
+        }
+        return Integer.parseInt(String.join("",strings));
     }
+
     public static boolean find(List<Integer> list){
         boolean flag = true;
         for (int i = 0; i < list.size()-1; i++) {
