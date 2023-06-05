b9cd913c0f9851830d550ef012b43c4e5b690da7
hyq19 <10775197514@qq.com>
2023-06-05 15:11:51 +0800

    动态规划，没有弄懂背包规划问题

---
+++ src/com/huang/medium/canPartition/Solution.java
@@ -0,0 +1,52 @@
+package com.huang.medium.canPartition;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+//        int[] nums = {1,2,2,3};
+//        int[] nums = {1,2,5};
+//        int[] nums = {14,9,8,4,3,2};
+//        int[] nums = {1,1};
+        int[] nums = {9,1,2,4,10};
+//        int[] nums = {1,2,3,4,5,6,7};
+        System.out.println(canPartition(nums));
+    }
+    public static boolean canPartition(int[] nums) {
+        int[][] dp = new int[nums.length][nums.length];
+        int sum = Arrays.stream(nums).sum();
+//        Arrays.sort(nums);
+        if(sum % 2 == 1){
+            return false;
+        }
+        sum = sum/2;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+
+                if (i == j ) {
+                    dp[i][j] = nums[i];
+                    if (dp[i][j] == sum) {
+                        return true;
+                    }
+                    continue;
+                }
+                if (j == 0) dp[i][j] = nums[i] + nums[j];
+                if (j > 0) {
+                    if (i == j) {
+                        dp[i][j] = dp[i][j - 1];
+                    } else {
+                        dp[i][j] = dp[i][j - 1] + nums[j];
+                    }
+                }
+                if (dp[i][j] > sum) {
+                    dp[i][j] = dp[i][j] - nums[j];
+                    continue;
+                }
+                if (dp[i][j] == sum) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
