76acc6076fabd873bf14a5484385b9c8f4234ce1
hyq19 <10775197514@qq.com>
2023-06-06 14:11:12 +0800

    动态规划，背包问题，目标和，还是将问题转换为背包问题没有成功

--- src/com/huang/medium/canPartition/Solution.java
+++ src/com/huang/medium/canPartition/Solution.java
@@ -13,40 +13,61 @@ public class Solution {
         System.out.println(canPartition(nums));
     }
     public static boolean canPartition(int[] nums) {
-        int[][] dp = new int[nums.length][nums.length];
         int sum = Arrays.stream(nums).sum();
-//        Arrays.sort(nums);
         if(sum % 2 == 1){
             return false;
         }
-        sum = sum/2;
+        int target = sum/2;
+        int[] dp = new int[target+1];
+        dp[0] = 0;
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
 
-                if (i == j ) {
-                    dp[i][j] = nums[i];
-                    if (dp[i][j] == sum) {
-                        return true;
-                    }
-                    continue;
-                }
-                if (j == 0) dp[i][j] = nums[i] + nums[j];
-                if (j > 0) {
-                    if (i == j) {
-                        dp[i][j] = dp[i][j - 1];
-                    } else {
-                        dp[i][j] = dp[i][j - 1] + nums[j];
-                    }
-                }
-                if (dp[i][j] > sum) {
-                    dp[i][j] = dp[i][j] - nums[j];
-                    continue;
-                }
-                if (dp[i][j] == sum) {
-                    return true;
-                }
+            for (int j = target; j >= nums[i]; j--) {
+//                if(i < j){
+//                    dp[j] = Math.max(dp[j], nums[j]);
+//                }else {
+                    dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+//                }
             }
         }
-        return false;
+        return dp[target] == target;
     }
+
+//    public static boolean canPartition(int[] nums) {
+//        int[][] dp = new int[nums.length][nums.length];
+//        int sum = Arrays.stream(nums).sum();
+////        Arrays.sort(nums);
+//        if(sum % 2 == 1){
+//            return false;
+//        }
+//        sum = sum/2;
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = i; j < nums.length; j++) {
+//
+//                if (i == j ) {
+//                    dp[i][j] = nums[i];
+//                    if (dp[i][j] == sum) {
+//                        return true;
+//                    }
+//                    continue;
+//                }
+//                if (j == 0) dp[i][j] = nums[i] + nums[j];
+//                if (j > 0) {
+//                    if (i == j) {
+//                        dp[i][j] = dp[i][j - 1];
+//                    } else {
+//                        dp[i][j] = dp[i][j - 1] + nums[j];
+//                    }
+//                }
+//                if (dp[i][j] > sum) {
+//                    dp[i][j] = dp[i][j] - nums[j];
+//                    continue;
+//                }
+//                if (dp[i][j] == sum) {
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+//    }
 }
